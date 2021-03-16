f9dbf8fe756d27ad204dad4c5f75a9f268315c44
FatK <officialteamxcraft@gmail.com>
2021-03-16 20:22:34 +0700

    Update grid and class

---
+++ list.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./assets/style/style.css" rel="stylesheet">
+    <link href="./assets/style/list.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="mt-3 mb-3">
+            <h3>Explore Movie</h3>
+            <form>
+                <div class="input-group mt-1">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text"><svg width="25" height="25" class="svgIcon-use" viewBox="0 0 25 25" data-children-count="0">
+                                <path d="M20.067 18.933l-4.157-4.157a6 6 0 10-.884.884l4.157 4.157a.624.624 0 10.884-.884zM6.5 11c0-2.62 2.13-4.75 4.75-4.75S16 8.38 16 11s-2.13 4.75-4.75 4.75S6.5 13.62 6.5 11z"></path>
+                            </svg></span>
+                    </div>
+                    <input placeholder="Search your movie..." class="search" />
+                </div>
+            </form>
+        </div>
+        <div class="list__wrapper">
+            <div class=" item__wrapper box-shadow">
+                <div class="item__image">
+                    <img src="https://media.kitsu.io/anime/poster_images/8736/large.jpg?1597697638">
+                </div>
+                <div class="item__detail">
+                    <h3>Owari no Seraph</h3><small class="score"> 5*</small>
+                    <p class="year">2014</p>
+                    <p class="mb-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem porro, in labore consequatur, iusto reprehenderit ea, delectus dicta alias similique tempore voluptates architecto. Assumenda exercitationem commodi consequatur labore autem earum.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem porro, in labore consequatur, iusto reprehenderit ea, delectus dicta alias similique tempore voluptates architecto. Assumenda exercitationem commodi consequatur labore autem earum.
+                    </p>
+                    <div class="">
+                        <a class="btn btn-primary">Read More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
